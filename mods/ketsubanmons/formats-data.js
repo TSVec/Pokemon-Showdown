ebffd262fc4d5c2476b7d4d43b5b79a2cf932b22
TSVec <vecstardom@gmail.com>
2016-12-23 09:36:07 -0600

    Update formats-data.js

--- mods/ketsubanmons/formats-data.js
+++ mods/ketsubanmons/formats-data.js
@@ -4,13 +4,4 @@ exports.BattleFormatsData = {
 		isNonstandard: false,
 		tier: "OU"
 	},
-	feraligatrmega: {
-		randomBattleMoves: ["aquajet","waterfall","crunch","icepunch","dragondance","swordsdance","earthquake"],
-		randomDoubleBattleMoves: ["aquajet","waterfall","crunch","icepunch","dragondance","swordsdance","earthquake","protect"],
-		requiredItem: "Venusaurite",
-		eventPokemon: [
-			{"generation":6,"level":50,"isHidden":true,"moves":["icepunch","crunch","waterfall","screech"]}
-		],
-		tier: "OU"
-	}
 };
