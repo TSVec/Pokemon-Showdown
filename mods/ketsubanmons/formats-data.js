1b7af3920cc4af12475fd23daca3bf04926b1c3f
TSVec <vecstardom@gmail.com>
2016-12-23 08:52:39 -0600

    Add files via upload

---
+++ mods/ketsubanmons/formats-data.js
@@ -0,0 +1,16 @@
+exports.BattleFormatsData = {
+	toxeon: {
+		randomBattleMoves: ["baneburst","return","curse","rest"],
+		isNonstandard: false,
+		tier: "OU"
+	},
+	feraligatrmega: {
+		randomBattleMoves: ["aquajet","waterfall","crunch","icepunch","dragondance","swordsdance","earthquake"],
+		randomDoubleBattleMoves: ["aquajet","waterfall","crunch","icepunch","dragondance","swordsdance","earthquake","protect"],
+		requiredItem: "Venusaurite",
+		eventPokemon: [
+			{"generation":6,"level":50,"isHidden":true,"moves":["icepunch","crunch","waterfall","screech"]}
+		],
+		tier: "OU"
+	}
+};
