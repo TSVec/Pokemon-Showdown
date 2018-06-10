351578dbc5745ed0881df6353986115cf42a9449
Retrosol <r3trosol@gmail.com>
2018-06-09 21:02:38 -0500

    Update moves.js

--- mods/prism/moves.js
+++ mods/prism/moves.js
@@ -271,9 +271,9 @@ exports.BattleMovedex = {
         	pp: 20,
         	priority: 0,
         	flags: {protect: 1, mirror: 1},
-        	onEffectiveness: function (typeMod, type) {
-            		if (type === 'Ground') return 1;
-        	},
+        	///onEffectiveness: function (typeMod, type) {
+         ///   		if (type === 'Ground') return 1;
+        ///	},
         	secondary: false,
         	target: "normal",
         	type: "Electric",
