42c4abfb94985fabd8a7e9c97e3eff9a6a4b0905
Retrosol <r3trosol@gmail.com>
2018-06-11 08:03:48 -0500

    Revert Rapid Spin Changes

--- mods/prism/moves.js
+++ mods/prism/moves.js
@@ -1490,7 +1490,7 @@ contestType: "Cool",
 				if (pokemon.hp && pokemon.removeVolatile('leechseed')) {
 					this.add('-end', pokemon, 'Leech Seed', '[from] move: Rapid Spin', '[of] ' + pokemon);
 				}
-				let sideConditions = ['lavapool', 'spikes', 'toxicspikes', 'stealthrock', 'stickyweb'];
+				let sideConditions = ['spikes', 'toxicspikes', 'stealthrock', 'stickyweb'];
 				for (const condition of sideConditions) {
 					if (pokemon.hp && pokemon.side.removeSideCondition(condition)) {
 						this.add('-sideend', pokemon.side, this.getEffect(condition).name, '[from] move: Rapid Spin', '[of] ' + pokemon);
