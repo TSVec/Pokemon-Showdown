66e150c7d78196477174defc994d1713d99b3cb9
Retrosol <r3trosol@gmail.com>
2018-06-11 08:02:35 -0500

    Added Competitive to Prism

--- mods/prism/abilities.js
+++ mods/prism/abilities.js
@@ -13,6 +13,29 @@ exports.BattleAbilities = {
 			}
 		},
 	},
+	"competitive": {
+		desc: "This Pokemon's Special Attack is raised by 2 stages for each of its stat stages that is lowered by an opposing Pokemon.",
+		shortDesc: "This Pokemon's Sp. Atk is raised by 2 for each of its stats that is lowered by a foe.",
+		onAfterEachBoost: function (boost, target, source) {
+			if (!source || target.side === source.side) {
+				return;
+			}
+			let statsLowered = false;
+			for (let i in boost) {
+				// @ts-ignore
+				if (boost[i] < 0) {
+					statsLowered = true;
+				}
+			}
+			if (statsLowered) {
+				this.boost({spa: 2}, target, target, null, true);
+			}
+		},
+		id: "competitive",
+		name: "Competitive",
+		rating: 2.5,
+		num: 172,
+},
 	"effectspore": {
 		inherit: true,
 		onAfterDamage: function (damage, target, source, move) {
@@ -126,7 +149,7 @@ exports.BattleAbilities = {
 		desc: "This Pokemon's moves that match one of its types have a same-type attack bonus (STAB) of 3 instead of 1.5 while in Naljo.",
 		shortDesc: "This Pokemon's same-type attack bonus (STAB) is 3 instead of 1.5.",
 		onModifyMove: function (move) {
-			move.stab = 3;
+			move.stab = 2.5;
 		},
 		id: "naljofury",
 		name: "Naljo Fury",
